b150b6b8c6d93466077836f1b3aeccd51af5abef
you@example.com <mthao14@outlook.com>
2021-12-19 23:05:09 -0600

    start of pw function

--- Develop/script.js
+++ Develop/script.js
@@ -1,32 +1,48 @@
 // Assignment code here
 
+
+// prompts that pop up to create password
+
+// length prompt & confirmation
 var length = prompt("Select length of password between 8-128 characters.");
 
-window.alert(" You selected " + length + " characters. ");
+if (length > 8 && length < 128) {
+  window.alert(" You selected " + length + " characters. ");
 
-var characterType = prompt("Enter at least 1 character type: lowercase, uppercase, numeric, special character (! @ # $ % * + - ?).");
+} else { 
+  window.alert("Please enter value between 8-128 for characters.");
+}
+
+// special character prompt & confirmation
+var characterType = prompt("Enter at least 1 character type for password: lowercase, uppercase, numeric, special character (! @ # $ % * + - ?).");
 
 window.alert("You selected " + characterType + " for your special character(s). ");
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+// function declaration
+function writePassword(max,min) {
+  var passwordChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#@!%&()/";
+  var randPwLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  var randPassword = Array(randPwLen).fill(passwordChars).map(function(x) { return x[Math.floor(Math.random() * x.length)] }).join('');
+  return randPassword;
+}
+  
   var passwordText = document.querySelector("#password");
 
-
-  passwordText.value = password;
-
-}
+  passwordText.value = writePassword;
 
 // Add event listener to generate button
-document.querySelector('#generate.btn').addEventListener('click', generatedPassword);
+document.querySelector('#generate.btn').addEventListener('click', writePassword);
 
-function generatedPassword() {
-  window.alert(" Your password is " + password " . ");
+
+function writePassword() {
+  window.alert(" Your password is " + password + " . ");
 }
+
 
 
 
